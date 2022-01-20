d6389bf08d5b98aabe978e4bad634dffaa867500
zzmeyz89 <zzmeyz@mail.ru>
2022-01-20 11:03:47 +0300

    feat: add reverse function v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let straight = String(Math.abs(n)).split('');
+    let rev  = '';
+    for (let i = straight.length-1; i>=0; i--) {
+      rev += straight[i];
+    }
+  return +rev;
 }
+
